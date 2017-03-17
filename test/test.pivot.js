362564ec227288bee598b9391665ac3011ddf4b9
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2017-03-17 13:03:27 +0100

    tests: add more tests on pivot data

--- test/test.pivot.js
+++ test/test.pivot.js
@@ -102,3 +102,60 @@ test('instance - pivot - embed', (t) => {
       t.end()
     })
 })
+
+test('instance - pivot - with multiple models', (t) => {
+  t.plan(4)
+
+  let Foo
+  let Bar
+  let Baz
+  let foo
+  let bar
+  let baz
+
+  dropDb()
+    .then(() => {
+      Foo = requelize.model('foo')
+
+      Bar = requelize.model('bar')
+
+      Baz = requelize.model('baz')
+
+      Foo.belongsToMany('bar', 'bars')
+      Bar.belongsToMany('foo', 'foos')
+
+      Baz.belongsToMany('bar', 'bars')
+      Bar.belongsToMany('baz', 'bazes')
+
+      return requelize.sync()
+    })
+    .then(() => {
+      foo = new Foo({})
+      bar = new Bar({})
+      baz = new Baz({})
+
+      bar.foos = [ foo ]
+      bar.bazes = [ baz ]
+
+      foo.setPivot('bar', { data: 'pivot' })
+      baz.setPivot('bar', { data: 'pivotForBaz' })
+
+      return bar.saveAll({
+        bazes: true,
+        foos: true
+      })
+    })
+    .then(() => Bar.embed({ bazes: true, foos: true }))
+    .then((res) => {
+      t.ok(Array.isArray(res[0].bazes), 'has bars')
+      t.ok(Array.isArray(res[0].foos), 'has bars')
+      t.equal('pivotForBaz', res[0].bazes[0].getPivot('bar').data)
+      t.equal('pivot', res[0].foos[0].getPivot('bar').data)
+    })
+    .catch((err) => {
+      t.fail(err)
+    })
+    .then(() => {
+      t.end()
+    })
+})
