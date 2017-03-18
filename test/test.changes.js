842403ebc0cfab4db8107d3c05a6d9fac46d5fa1
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2017-03-18 11:17:38 +0100

    feat: changefeeds with observables

---
+++ test/test.changes.js
@@ -0,0 +1,67 @@
+const Joi = require('joi')
+
+const { test, requelize, dropDb } = require('./utils')
+
+test('changefeeds', (t) => {
+  t.plan(8)
+
+  let Foo
+  let foo
+
+  dropDb()
+    .then(() => {
+      Foo = requelize.model('foo', {
+        name: Joi.string()
+      })
+
+      return requelize.sync()
+    })
+    .then(() => {
+      let feed = Foo.feed()
+
+      feed
+        .filter(change => change.type === 'create')
+        .subscribe((change) => {
+          t.equal('create', change.type, 'subscribe to create')
+          t.equal('bar', change.to.name, 'created document')
+        })
+
+      feed
+        .filter(change => change.type === 'update')
+        .subscribe((change) => {
+          t.equal('update', change.type, 'subscribe to update')
+          t.equal('bar', change.from.name, 'updated document - from')
+          t.equal('baz', change.to.name, 'updated document - to')
+        })
+
+      feed
+        .filter(change => change.type === 'delete')
+        .subscribe((change) => {
+          t.equal('delete', change.type, 'subscribe to delete')
+          t.equal('baz', change.from.name, 'deleted document')
+        })
+
+      setTimeout(() => {
+        foo = new Foo({ name: 'bar' })
+        foo.save()
+      }, 100)
+
+      setTimeout(() => {
+        foo.name = 'baz'
+        foo.save()
+      }, 750)
+
+      setTimeout(() => {
+        foo.delete()
+      }, 1000)
+
+      setTimeout(() => {
+        feed.close()
+        t.pass('unsubscribed from changes')
+        t.end()
+      }, 2000)
+    })
+    .catch((err) => {
+      t.fail(err)
+    })
+})
---
+++ test/test.delete.js
@@ -0,0 +1,41 @@
+const Joi = require('joi')
+
+const { test, requelize, dropDb } = require('./utils')
+
+test('deletion', (t) => {
+  t.plan(2)
+
+  let Foo
+  let foo
+
+  dropDb()
+    .then(() => {
+      Foo = requelize.model('foo', {
+        name: Joi.string()
+      })
+
+      return requelize.sync()
+    })
+    .then(() => {
+      foo = new Foo({ name: 'bar' })
+
+      return foo.save()
+    })
+    .then(() => {
+      return foo.delete()
+    })
+    .then(() => {
+      t.equal(null, foo.id, 'deleted document')
+
+      return Foo.run()
+    })
+    .then((res) => {
+      t.equal(0, res.length, 'ensure deleted documents')
+    })
+    .catch((err) => {
+      t.fail(err)
+    })
+    .then(() => {
+      t.end()
+    })
+})
