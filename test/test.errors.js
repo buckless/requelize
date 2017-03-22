557dcc8fd84ef72a3b104f2a28eb8bace4a8956f
Gabriel JUCHAULT <gjuchault@users.noreply.github.com>
2017-03-23 00:43:11 +0100

    feat(errors): document not found error when parsing non-existent results (#14)
    * feat(errors): document not found error when parsing non-existent results
    
    * chore: version bump

---
+++ test/test.errors.js
@@ -0,0 +1,22 @@
+const { test, requelize, dropDb } = require('./utils')
+
+test('document not found error', (t) => {
+  t.plan(2)
+
+  let Foo
+
+  dropDb()
+    .then(() => {
+      Foo = requelize.model('foo')
+
+      return requelize.sync()
+    })
+    .then(() => {
+      return Foo.get('foo').run()
+    })
+    .catch((err) => {
+      t.equal('RequelizeError', err.name)
+      t.equal('DocumentNotFound', err.message)
+      t.end()
+    })
+})
